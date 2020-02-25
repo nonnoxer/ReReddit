c516268d3621b3bf2b0a9cee49583249d8e86509
NATANAEL TAN TIONG OON <22YNATA978H@student.ri.edu.sg>
2020-02-25 16:46:00 +0800

    scraper done

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import pandas as pd
 import praw
 
-reddit = praw.Reddit(client_id='WjcoN3aHxXLwUA', client_secret='ZFOnZNObuAFuTiJw_DHkOnTOAlI', user_agent='showerthoughts_scraper')
+def scrape(client_id, client_secret, user_agent, subreddit):
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent,
+    )
 
-posts = []
-sr_showerthoughts = reddit.subreddit('Showerthoughts')
-for post in sr_showerthoughts.top(limit=10):
-    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
-posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
-print(posts)
+    file_sr = open("r_{subreddit}.txt".format(subreddit=subreddit), "w+")
+    sr = reddit.subreddit(subreddit)
+    for post in sr.top(limit=10000):
+        file_sr.write(
+            "{title};;{score};;{id};;{subreddit};;{url};;{num_comments};;{selftext};;{created}\r".format(
+                title=post.title.encode("unicode_escape"),
+                score=post.score,
+                id=post.id,
+                subreddit=post.subreddit,
+                url=post.url,
+                num_comments=post.num_comments,
+                selftext=post.selftext.encode("unicode_escape"),
+                created=post.created,
+            )
+        )
+    file_sr.close()
+
+def read_scrape(subreddit):
+    file_sr = open("r_{subreddit}.txt".format(subreddit=subreddit), "r")
+    posts = []
+    posts = file_sr.read().split("\r")
+    for i in range(len(posts)):
+        posts[i] = posts[i].split(";;")
+scrape("WjcoN3aHxXLwUA", "ZFOnZNObuAFuTiJw_DHkOnTOAlI", "showerthoughts_scraper", "Showerthoughts")
+read_scrape("Showerthoughts")
+scrape("MN_P-UrbyJE4lg", "4zlX2qSuNU6dG_Qd_WiZNZlfE_o", "programmerhumor_scraper", "ProgrammerHumor")
+read_scrape("ProgrammerHumor")
